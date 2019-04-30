3772b58906f940478468437c400bfe4436535bb2
hydraChaosu <abmallim@gmail.com>
2019-04-30 18:45:46 +0200

    Add to primes generator, sum primes
    and removes a bug which occur when prime is a given number

--- primesGenerator.js
+++ primesGenerator.js
@@ -1,6 +1,6 @@
 const newMaxPrimeList = number => {
   let PrimesList = [2];
-  for (let i = 3; i < number; i++) {
+  for (let i = 3; i <= number; i++) {
     // console.log('lista',PrimesList,'sprawdzana liczba',i,'jej pierwiastek 2 stopnia', Math.sqrt(i))
     // i jak jej pierwiastek jest wiekszy od liczby pierwszej
     // to trzeba sprzwdzie wszystkie pierwsze i mniej od niej
@@ -25,3 +25,8 @@ const newMaxPrimeList = number => {
   }
   return PrimesList;
 };
+
+function sumPrimes(num) {
+  console.log(newMaxPrimeList(num).reduce((acc, red) => acc + red));
+  return newMaxPrimeList(num).reduce((acc, red) => acc + red);
+}
