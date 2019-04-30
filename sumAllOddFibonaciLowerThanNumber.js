8b84e819e309ed49e5ea06ab0100a8bf110025ab
hydraChaosu <abmallim@gmail.com>
2019-04-30 16:28:35 +0200

    Modify fibonaci

--- sumAllOddFibonaciLowerThanNumber.js
+++ sumAllOddFibonaciLowerThanNumber.js
@@ -1,11 +1,11 @@
 function sumFibs(num) {
   let result = 0;
   let fibonaci = 1;
-  let prevfibonaci = 1;
+  let prevfibonaci = 0;
   result = fibonaci + prevfibonaci;
   if (num === 1) return 1;
 
-  for (let i = 2; i < num; i++) {
+  for (let i = 1; i < num; i++) {
     fibonaci = fibonaci + prevfibonaci;
     prevfibonaci = fibonaci - prevfibonaci;
     if (fibonaci % 2 === 1 && fibonaci <= num) {
