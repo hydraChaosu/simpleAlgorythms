e9a80eb80f61918807c43aeca1020da20548e817
hydraChaosu <abmallim@gmail.com>
2019-05-08 14:17:36 +0200

    Add more optimalized primes generator

--- primesGenerator.js
+++ primesGenerator.js
@@ -30,3 +30,40 @@ function sumPrimes(num) {
   console.log(newMaxPrimeList(num).reduce((acc, red) => acc + red));
   return newMaxPrimeList(num).reduce((acc, red) => acc + red);
 }
+
+//zrob to z for i uzyciem break
+//wersja map z jedna iteracja
+
+const newMaxPrimeList2 = number => {
+  let PrimesList = [2];
+  for (let i = 3; i <= number; i++) {
+    let tempPrimes = [];
+    let tempListPrimes = [];
+    for (let j = 0; j < PrimesList.length; j++) {
+      // console.log(Math.sqrt(i) >= PrimesList[j], i, j);
+      if (Math.sqrt(i) >= PrimesList[j]) {
+        tempPrimes = tempPrimes.concat(PrimesList[j]);
+      } else {
+        break;
+      }
+    }
+    // console.log(tempPrimes);
+    for (let k = 0; k < tempPrimes.length; k++) {
+      if (i % tempPrimes[k] === 0) {
+        tempListPrimes = tempListPrimes.concat(1);
+        break;
+      } else {
+        tempListPrimes = tempListPrimes.concat(0);
+      }
+    }
+    // console.log(tempListPrimes.length);
+    if (tempListPrimes.every(number => number !== 1)) {
+      PrimesList.push(i);
+    }
+  }
+  return PrimesList;
+};
+
+console.log(newMaxPrimeList2(100));
+// console.log(newMaxPrimeList2(100).length); //
+// console.log(newMaxPrimeList(100).length); //
